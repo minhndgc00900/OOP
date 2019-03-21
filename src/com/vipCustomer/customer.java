e138f763b0c35247c25c5ebac5cf0f0850a133ce
minhndgc00900 <minhnd0496@gmail.com>
2019-03-21 20:03:42 +0700

    constructor 2 - challenge

---
+++ src/com/vipCustomer/customer.java
@@ -0,0 +1,36 @@
+package com.vipCustomer;
+
+public class customer
+{
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public customer() {
+        this("Minh", 423.00, "minh@abc.com");
+
+        System.out.println("we have " + name + " and " + creditLimit + " and " + email);
+    }
+
+    public customer(String name, double creditLimit) {
+        this(name, creditLimit, "lam@abc.com");
+    }
+
+    public customer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
---
+++ src/com/vipCustomer/main.java
@@ -0,0 +1,14 @@
+package com.vipCustomer;
+
+public class main
+{
+    public static void main(String[] args) {
+        customer cus = new customer();
+        customer cus2 = new customer("lam", 2312.00);
+        customer cus3 = new customer("tuan", 212.00, "ghj@abc.com");
+
+        System.out.println( "we have " + cus2.getName() + " and " + cus2.getCreditLimit() + " and " + cus2.getEmail());
+        System.out.println( "we have " + cus3.getName() + " and " + cus3.getCreditLimit() + " and " + cus3.getEmail());
+
+    }
+}
